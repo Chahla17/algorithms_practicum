1fdb5e2f0dcc1135ab7e02e10d244f2056ef0cfa
Chahla17 <93518227+Chahla17@users.noreply.github.com>
2022-11-01 00:09:21 +0300

    Update fib_big_even_odd.py

--- fib_big_even_odd.py
+++ fib_big_even_odd.py
@@ -1,4 +1,3 @@
-import timeit
 def fib_eo(n):
     previous, fib_number = 0, 1
     for _ in range(2, n + 1):
@@ -6,6 +5,5 @@ def fib_eo(n):
 
     return 'even' if fib_number % 2 == 0 else 'odd' 
 n = int(input('N Fibonacci number: > '))
-#1print(f'Result is: {fib_eo(n)}')
-time = timeit.timeit(lambda: fib_eo(n), number=1)
-print(time)
+print(f'Result is: {fib_eo(n)}')
+
