54da7ba794b4d7f8fac9819e804d86226019cff6
Chahla17 <93518227+Chahla17@users.noreply.github.com>
2022-11-01 00:09:53 +0300

    Update fib_binet.py

--- fib_binet.py
+++ fib_binet.py
@@ -1,4 +1,3 @@
-import timeit
 def fib(n):
 
     a = (1 + 5 ** 0.5) / 2
@@ -6,6 +5,5 @@ def fib(n):
     return (a ** n - b ** n) / 5 ** 0.5
 
 n = int(input('N Fibonacci number: > '))
-#print(f'Result is: {int(fib(n))}')
-time = timeit.timeit(lambda: fib(n), number=1)
-print(time)
+print(f'Result is: {int(fib(n))}')
+
